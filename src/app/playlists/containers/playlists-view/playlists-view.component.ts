3eee2512a3ef5ac263ee8855016dc03f4e5640c0
Mateusz Kulesza <ev45ive@gmail.com>
2021-12-20 15:50:41 +0100

    Showing and hiding elements based on state (state machine)

--- src/app/playlists/containers/playlists-view/playlists-view.component.ts
+++ src/app/playlists/containers/playlists-view/playlists-view.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+// enum Modes { details, form }
+
 @Component({
   selector: 'app-playlists-view',
   templateUrl: './playlists-view.component.html',
   styleUrls: ['./playlists-view.component.scss']
 })
 export class PlaylistsViewComponent implements OnInit {
+  // Modes = Modes
+  // mode = Modes.details
+
+  mode: 'details' | 'edit' = 'details'
 
   constructor() { }
+
+  editMode() {
+    this.mode = 'edit'
+  }
+
+  cancel() {
+    this.mode = 'details'
+  }
 
   ngOnInit(): void {
   }
