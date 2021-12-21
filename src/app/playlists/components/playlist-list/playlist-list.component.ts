c1f449a3eee8005c77e29bfaffa3f619eaf571a6
Mateusz Kulesza <ev45ive@gmail.com>
2021-12-21 10:49:42 +0100

    playlists list - ngFor ngForOf ngForOfContext + $implicit + let

--- src/app/playlists/components/playlist-list/playlist-list.component.ts
+++ src/app/playlists/components/playlist-list/playlist-list.component.ts
@@ -1,4 +1,9 @@
+import { NgForOf, NgForOfContext, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+
+NgIf
+NgForOf
+NgForOfContext  // => $implicit
 
 @Component({
   selector: 'app-playlist-list',
@@ -6,6 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./playlist-list.component.scss']
 })
 export class PlaylistListComponent implements OnInit {
+
+  playlists = [{
+    id: '123',
+    name: 'Best playlist 123',
+    public: true,
+    description: 'awesome playlist'
+  }, {
+    id: '234',
+    name: 'Best playlist 234',
+    public: false,
+    description: 'my <3 playlist'
+  }, {
+    id: '345',
+    name: 'Best playlist 345',
+    public: true,
+    description: 'my last playlist'
+  }]
+
 
   constructor() { }
 
