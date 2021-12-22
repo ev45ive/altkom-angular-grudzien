0eac37f905543031175836388f27010f345a1e57
Mateusz Kulesza <ev45ive@gmail.com>
2021-12-22 09:12:01 +0100

    environment / enironment.prod

--- src/app/core/services/search.service.ts
+++ src/app/core/services/search.service.ts
@@ -3,18 +3,26 @@ import { mockAlbums } from '../mocks/albums';
 import { HttpClient } from '@angular/common/http'
 import { AlbumItemView } from '../model/album';
 
+/// can be replaced when compiling:
+import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root',
 })
 export class SearchService {
+
+  api_url = environment
 
   constructor(
     private http: HttpClient
   ) { }
 
   searchAlbums(query: string) {
-    const res = this.http.get<AlbumItemView[]>(`assets/albums.json`, {
-      params: {}, headers: {},
+    const res = this.http.get<AlbumItemView[]>(this.api_url + 'search', {
+      params: {
+        type: 'album'
+      },
+      headers: {},
     })
 
     return res
